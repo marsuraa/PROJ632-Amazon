c2c095000d1ab4699cb9b132a51f60425edb91da
marsuraa <tony2parker0@gmail.com>
2019-05-16 08:20:15 +0200

    add

--- AmazonReview scrapper.py
+++ AmazonReview scrapper.py
@@ -25,7 +25,6 @@ def getAllReviews(asin):
             return {"url": amazon_url, "error": "page not found"}
         if response.status_code != 200:
             continue
-        
         # Removing the null bytes from the response.
         cleaned_response = response.text.replace('\x00', '')
         
@@ -33,12 +32,13 @@ def getAllReviews(asin):
 
         #XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
 
-        XPATH_ALL_REVIEWS = '//a[@data-hook="see-all-reviews-link-foot"]/@href'
+        XPATH_ALL_REVIEWS = '//a[contains(@data-hook,"see-all-reviews-link-foot")]/@href'
 
 
         #raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
 
         all_reviews = parser.xpath(XPATH_ALL_REVIEWS)
+        #print(all_reviews[0])
 
         return all_reviews[0]
        
@@ -46,8 +46,10 @@ def getAllReviews(asin):
  
     
 def ParseReviews(url):
-    pageNumber=2
-    amazon_url  = 'http://www.amazon.com'+url
+    pageNumber=1
+    sleep(5)
+    amazon_url  = 'https://www.amazon.com'+url
+    print(amazon_url)
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
     for i in range(5):
         response = get(amazon_url, headers = headers, verify=False, timeout=30)
@@ -55,7 +57,6 @@ def ParseReviews(url):
             return {"url": amazon_url, "error": "page not found"}
         if response.status_code != 200:
             continue
-        
         # Removing the null bytes from the response.
         cleaned_response = response.text.replace('\x00', '')
         
@@ -63,15 +64,18 @@ def ParseReviews(url):
 
         #XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
 
-        XPATH_LAST = './/li[@class="a-last"]//a/@href'
+        XPATH_LAST = '//li[contains(@class,"last")]/a/@href'
+        XPATH_REVIEW_CONTAINER = '//div[contains(@id,"cm_cr-review_list")]'
 
 
         #raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
 
         last = parser.xpath(XPATH_LAST)
         print(last)
-        return last
-        while(last[0]!=''):
+        reviews_list=[]
+        while(last[0] != None):
+            sleep(10)
+            print("ok")
             # This script has only been tested with Amazon.com
             amazon_url  = 'http://www.amazon.com'+url+'&pageNumber='+str(pageNumber)
             # Add some recent user agent to prevent amazon from blocking the request 
@@ -88,42 +92,20 @@ def ParseReviews(url):
                 cleaned_response = response.text.replace('\x00', '')
                 
                 parser = html.fromstring(cleaned_response)
-                #XPATH_AGGREGATE = '//span[@id="acrCustomerReviewText"]'
-                #XPATH_REVIEW_SECTION_1 = '//div[contains(@id,"reviews-summary")]'
-                #XPATH_REVIEW_SECTION_2 = '//div[@data-hook="review"]'
-                #XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
                 XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
-                #XPATH_PRODUCT_PRICE = '//span[@id="priceblock_ourprice"]/text()'
-                XPATH_ALL_REVIEWS = '//a[@data-hook="see-all-reviews-link-foot"]/@href'
         
                 #raw_product_price = parser.xpath(XPATH_PRODUCT_PRICE)
                 raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
                 #total_ratings  = parser.xpath(XPATH_AGGREGATE_RATING)
-                #reviews = parser.xpath(XPATH_REVIEW_SECTION_1)
-                all_reviews = parser.xpath(XPATH_ALL_REVIEWS)
+                reviews = parser.xpath(XPATH_REVIEW_CONTAINER)
         
                 #product_price = ''.join(raw_product_price).replace(',', '')
                 product_name = ''.join(raw_product_name).strip()
         
-                #if not reviews:
-                 #   reviews = parser.xpath(XPATH_REVIEW_SECTION_2)
-                #ratings_dict = {}
-                #reviews_list = []
-        
-                # Grabing the rating  section in product page
-                ###
-                #for ratings in total_ratings:
-                 #   extracted_rating = ratings.xpath('./td//a//text()')
-                  #  if extracted_rating:
-                   #     rating_key = extracted_rating[0] 
-                    #    raw_raing_value = extracted_rating[1]
-                     #   rating_value = raw_raing_value
-                      #  if rating_key:
-                       #     ratings_dict.update({rating_key: rating_value})
-                ###
+                
             # Parsing individual reviews
                 for review in reviews:
-                    XPATH_RATING  = './/i[@data-hook="review-star-rating"]//text()'
+                    XPATH_RATING  = './/i[@data-hook="a-row a-spacing-none"]//text()'
                     XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//text()'
                     XPATH_REVIEW_POSTED_DATE = './/span[@data-hook="review-date"]//text()'
                     XPATH_REVIEW_TEXT_1 = './/div[@data-hook="review-collapsed"]//text()'
@@ -180,15 +162,154 @@ def ParseReviews(url):
                     reviews_list.append(review_dict)
     
             data = {
-                        'ratings': ratings_dict,
                         'reviews': reviews_list,
                         'url': amazon_url,
                         'name': product_name
                         #'price': product_price
                     
                     }
-            return data
-        return {"error": "failed to process the page", "url": amazon_url}
+        return data
+    return {"error": "failed to process the page", "url": amazon_url}
+    
+    
+    
+def ParseReviews2():
+    pageNumber=1
+    
+    amazon_url  = 'https://www.amazon.com/Samsung-Galaxy-J7-Contract-Carrier/product-reviews/B01ETPUQ6E?reviewerType=all_reviews'
+    print(amazon_url)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+    last=[]
+    reviews_list=[]
+    
+    while(not last):
+        response = get(amazon_url, headers = headers, verify=False, timeout=30)
+        if response.status_code == 404:
+            return {"url": amazon_url, "error": "page not found"}
+        if response.status_code != 200:
+            continue
+        # Removing the null bytes from the response.
+        cleaned_response = response.text.replace('\x00', '')
+        
+        parser = html.fromstring(cleaned_response)
+
+        #XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
+
+        XPATH_LAST = '//li[contains(@class,"last")]/a/@href'
+        
+
+
+        #raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
+
+        last = parser.xpath(XPATH_LAST)
+        print(last)
+        
+    while(last[0] != ''):
+        sleep(5)
+        print("ok")
+        # This script has only been tested with Amazon.com
+        amazon_url  = 'https://www.amazon.com/Samsung-Galaxy-J7-Contract-Carrier/product-reviews/B01ETPUQ6E?reviewerType=all_reviews&pageNumber='+str(pageNumber)
+        # Add some recent user agent to prevent amazon from blocking the request 
+        # Find some chrome user agent strings  here https://udger.com/resources/ua-list/browser-detail?browser=Chrome
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+        reviews = []
+        while(not reviews):
+            response = get(amazon_url, headers = headers, verify=False, timeout=30)
+            if response.status_code == 404:
+                return {"url": amazon_url, "error": "page not found"}
+            if response.status_code != 200:
+                continue
+            
+            print(response.status_code)
+            
+            # Removing the null bytes from the response.
+            cleaned_response = response.text.replace('\x00', '')
+            
+            parser = html.fromstring(cleaned_response)
+            XPATH_REVIEW_SECTION_1 = '//div[@data-hook="review"]'
+            XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
+    
+            raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
+        
+            reviews = parser.xpath(XPATH_REVIEW_SECTION_1)
+            product_name = ''.join(raw_product_name).strip()
+    
+            print("bug")
+            print(reviews)
+
+        
+    # Parsing individual reviews
+        
+        for review in reviews:
+            print("on rentre")
+            XPATH_RATING  = './/i[@data-hook="a-row a-spacing-none"]//text()'
+            XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//text()'
+            XPATH_REVIEW_POSTED_DATE = './/span[@data-hook="review-date"]//text()'
+            XPATH_REVIEW_TEXT_1 = './/span[@data-hook="review-body"]//text()'
+            XPATH_REVIEW_TEXT_2 = './/div//span[@data-action="columnbalancing-showfullreview"]/@data-columnbalancing-showfullreview'
+            XPATH_REVIEW_COMMENTS = './/span[@data-hook="review-comment"]//text()'
+            XPATH_AUTHOR = './/span[contains(@class,"profile-name")]//text()'
+            XPATH_AUTHOR_LINK = './/a[contains(@class,"profile")]/@href'
+            XPATH_REVIEW_TEXT_3 = './/div[contains(@id,"dpReviews")]/div/text()'
+            
+            raw_review_author = review.xpath(XPATH_AUTHOR)
+            raw_review_author_link = review.xpath(XPATH_AUTHOR_LINK)
+            raw_review_rating = review.xpath(XPATH_RATING)
+            raw_review_header = review.xpath(XPATH_REVIEW_HEADER)
+            raw_review_posted_date = review.xpath(XPATH_REVIEW_POSTED_DATE)
+            raw_review_text1 = review.xpath(XPATH_REVIEW_TEXT_1)
+            raw_review_text2 = review.xpath(XPATH_REVIEW_TEXT_2)
+            raw_review_text3 = review.xpath(XPATH_REVIEW_TEXT_3)
+            
+            print(raw_review_author)
+
+            # Cleaning data
+            author = ' '.join(' '.join(raw_review_author).split())
+            review_rating = ''.join(raw_review_rating).replace('out of 5 stars', '')
+            review_header = ' '.join(' '.join(raw_review_header).split())
+
+            try:
+                review_posted_date = dateparser.parse(''.join(raw_review_posted_date)).strftime('%d %b %Y')
+            except:
+                review_posted_date = None
+            review_text = ' '.join(' '.join(raw_review_text1).split())
+
+            # Grabbing hidden comments if present
+            if raw_review_text2:
+                json_loaded_review_data = loads(raw_review_text2[0])
+                json_loaded_review_data_text = json_loaded_review_data['rest']
+                cleaned_json_loaded_review_data_text = sub('<.*?>', '', json_loaded_review_data_text)
+                full_review_text = review_text+cleaned_json_loaded_review_data_text
+            else:
+                full_review_text = review_text
+            if not raw_review_text1:
+                full_review_text = ' '.join(' '.join(raw_review_text3).split())
+
+            raw_review_comments = review.xpath(XPATH_REVIEW_COMMENTS)
+            review_comments = ''.join(raw_review_comments)
+            review_comments = sub('[A-Za-z]', '', review_comments).strip()
+            review_dict = {
+                                'review_comment_count': review_comments,
+                                'review_text': full_review_text,
+                                'review_posted_date': review_posted_date,
+                                'review_header': review_header,
+                                'review_rating': review_rating,
+                                'review_author': author,
+                                'review_author_link': raw_review_author_link
+
+                            }
+            reviews_list.append(review_dict)
+            print (reviews_list)
+        last = parser.xpath(XPATH_LAST)
+
+    data = {
+                    'reviews': reviews_list,
+                    'url': amazon_url,
+                    'name': product_name
+                    #'price': product_price
+                
+                }
+    return data
 
 def ReadAsin():
     # Add your own ASINs here
@@ -197,7 +318,8 @@ def ReadAsin():
     
     for asin in AsinList:
         print("Downloading and processing page http://www.amazon.com/dp/" + asin)
-        extracted_data.append(ParseReviews(getAllReviews(asin)))
+        #extracted_data.append(ParseReviews(getAllReviews(asin)))
+        extracted_data.append(ParseReviews2())
         sleep(7)
     f = open('data.json', 'w')
     dump(extracted_data, f, indent=4)
